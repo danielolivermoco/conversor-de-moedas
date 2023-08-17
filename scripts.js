fa3160043c8ca41f2f40a6601a2a064a15b2f203
danielolivermoco <danielmoco10@gmail.com>
2023-08-17 13:43:39 -0300

    commit-convert-money

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+const convertButton = document.querySelector(".convert-button")
+
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector("input-currency").value
+
+    console.log("inputCurrencyValue")
+
+}
+
+
+convertButton.addEventListener("click", convertValues) 
