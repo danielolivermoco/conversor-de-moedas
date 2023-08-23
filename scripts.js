e57d383304ba51deb5686b067a682cf353941d3b
danielolivermoco <danielmoco10@gmail.com>
2023-08-23 12:11:08 -0300

    css-finalizado

--- scripts.js
+++ scripts.js
@@ -1,13 +1,30 @@
-
 const convertButton = document.querySelector(".convert-button")
-
+const currencySelect = document.querySelector(".currency-select")
 
 function convertValues() {
-    const inputCurrencyValue = document.querySelector("input-currency").value
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // Valor em Real
+    const currencyValueToConverted = document.querySelector(".currency-value") // Outras moedas 
 
-    console.log("inputCurrencyValue")
+    console.log(currencySelect.value)
+    const dolarToday = 5.2
+    const euroToday = 6.2
 
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+    currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue)
+
+    
 }
 
 
-convertButton.addEventListener("click", convertValues) 
+
+convertButton.addEventListener("click", convertValues)
