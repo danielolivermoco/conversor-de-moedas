8dfc2b5be7302e209d71b1e9ffb667f25736f05a
danielolivermoco <danielmoco10@gmail.com>
2024-03-06 17:48:46 -0300

    mudança imagem de fundo e consumo de uma API

--- scripts.js
+++ scripts.js
@@ -1,21 +1,22 @@
 const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-select")
 
-function convertValues() {
+const convertValues = async () => {
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // Valor em Real
     const currencyValueToConverted = document.querySelector(".currency-value") // Outras moedas 
 
-    const dolarToday = 5.2
-    const euroToday = 6.2
-   
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
 
     if (currencySelect.value == "Dolar") {
         // Se o select estiver selecionado o valor de dolar, entre aqui
         currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"
-        }).format(inputCurrencyValue / dolarToday)
+        }).format(inputCurrencyValue / dolar)
     }
 
     if (currencySelect.value == "Euro") {
@@ -23,7 +24,7 @@ function convertValues() {
         currencyValueToConverted.innerHTML = new Intl.NumberFormat("de-De", {
             style: "currency",
             currency: "EUR"
-        }).format(inputCurrencyValue / euroToday)
+        }).format(inputCurrencyValue / euro)
     }
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
